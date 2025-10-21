2461e674ee77e633649235d69711f03943874c23
Ayushi Sharma <ayusharma517@gmail.com>
2025-10-21 11:05:20 +0000

    Added variables and data types examples

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId=14453
+let accountEmail="ayushi@gmial.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+let accountState
+
+
+
+accountEmail="shobhit@gmail.com"
+accountCity="bengaluru"
+accountPassword="222"
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+/*
+
+*/
---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,34 @@
+"use strict"; //it will treat all JS code as newer verion of code
+
+ //alert("hello")  we are using nodejs to run,not browser..so it gives error
+
+ //in most files we avoid semicolon
+
+ console.log(3+3)
+ console.log(3+
+    3
+ ) //acceptable but code readability decreases
+ console.log("ayushi")
+
+ let name="ayushi" //string data typrs
+ let age=18 //numeric data type
+ let isLoggegIn=false //boolean data types
+ let state;  //undefined
+ let st=null //empty
+
+ //number=> Range=2 ^53
+ //bigInt=> for bigger numbers
+ //String =>""
+ //boolean=>true/false
+ //null=>standalone value
+ //undefined=>value is no yet assigned
+ //symbol=>used to find uniqueness
+
+
+ //object
+
+ console.log(typeof "ayushi"); //typeof tells about the type of object
+ console.log(typeof null);  //type of null is object
+console.log(typeof undefined) //type is undefined because undefined is type in js
+ 
+ 
